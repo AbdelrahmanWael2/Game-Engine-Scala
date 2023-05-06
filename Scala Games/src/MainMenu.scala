9141c32d25c7f369e435a7f95cc8d3c2e0d7a782
Ahmed Hesham <ahmedhesham.1652001@gmail.com>
2023-05-06 15:31:47 +0300

    fix multi windows

--- Scala Games/src/MainMenu.scala
+++ Scala Games/src/MainMenu.scala
@@ -3,12 +3,15 @@ import java.awt.event.{ActionEvent, ActionListener}
 import javax.swing._
 import TicTacToe.TicTacToeDrawer
 import TicTacToe.TicTacToeController
+import Connect.ConnectControler
+import Connect.ConnectDrawer
+import Queens.QueensController
+import Queens.QueensDrawer
 import sun.security.krb5.KrbException
 import sun.security.krb5.KrbException.errorMessage
 
 import java.util.Scanner
 import scala.collection.mutable.ArrayBuffer
-
 
 object MainMenu {
 
@@ -20,7 +23,7 @@ object MainMenu {
     val frame = new JFrame()
     frame.setVisible(true)
 
-    //rows, columns, h gap, v gap
+    // rows, columns, h gap, v gap
     var gridLayout = new GridLayout(10, 10, 10, 10)
     val mainPanel = new JPanel(gridLayout)
     val TicButton = new JButton("Tic-Tac-Toe")
@@ -29,7 +32,6 @@ object MainMenu {
     val ChessButton = new JButton("Chess")
     val SudokoButton = new JButton("Sudoku")
     val QueensButton = new JButton("8-Queens")
-
 
     mainPanel setBorder BorderFactory.createEmptyBorder(10, 10, 10, 10)
     mainPanel add TicButton;
@@ -54,27 +56,17 @@ object MainMenu {
       }
     }
 
-    //    val connectListener: ActionListener = new ActionListener {
-    //      def actionPerformed(e: ActionEvent): Unit = {
-    //        new Game(
-    //          "connect 4",
-    //          connect.controller,
-    //          connect.draw
-    //        )
-    //        setVisible(false)
-    //      }
-    //    }
+    val connectListener: ActionListener = new ActionListener {
+      def actionPerformed(e: ActionEvent): Unit = {
+        AbstractGameEngine(ConnectControler, ConnectDrawer)
+      }
+    }
     //
-    //    val queensListener: ActionListener = new ActionListener {
-    //      def actionPerformed(e: ActionEvent): Unit = {
-    //        new Game(
-    //          "queens",
-    //          queens.controller,
-    //          queens.draw
-    //        )
-    //        setVisible(false)
-    //      }
-    //    }
+    val queensListener: ActionListener = new ActionListener {
+      def actionPerformed(e: ActionEvent): Unit = {
+        AbstractGameEngine(QueensController, QueensDrawer)
+      }
+    }
     //    val chessListener: ActionListener = new ActionListener {
     //      def actionPerformed(e: ActionEvent): Unit = {
     //        new Game(
@@ -97,25 +89,31 @@ object MainMenu {
     //    }
 
     TicButton.addActionListener(ticListener)
-    //    ConnectButton.addActionListener(connectListener)
-    //    QueensButton.addActionListener(queensListener)
+    ConnectButton.addActionListener(connectListener)
+    QueensButton.addActionListener(queensListener)
     //    ChessButton.addActionListener(chessListener)
     //    SudokoButton.addActionListener(sudokoListener)
   }
 
-  def AbstractGameEngine(Controller: (ArrayBuffer[Int], (String, Int)) => (Boolean,ArrayBuffer[Int]),
-                         Drawer: ArrayBuffer[Int] => Unit): Unit = {
+  def AbstractGameEngine(
+      Controller: (
+          ArrayBuffer[Int],
+          (String, Int)
+      ) => (Boolean, ArrayBuffer[Int]),
+      Drawer: ArrayBuffer[Int] => Unit
+  ): Unit = {
 
     var turn = 0
     var state = ArrayBuffer.fill(9)(0)
 
+    Drawer(state)
     createInputWindow()
 
     def createInputWindow(): Unit = {
       val frame = new JFrame("Input Window")
       frame.setSize(400, 200)
       frame.setLayout(new BorderLayout())
-      frame.setLocation(800,400)
+      frame.setLocation(800, 400)
 
       val panel = new JPanel()
       panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS))
@@ -124,48 +122,34 @@ object MainMenu {
       panel.add(inputField)
 
       val submitButton = new JButton("Submit")
-      Drawer(state)
 
       //
       submitButton.addActionListener(new ActionListener() {
         def actionPerformed(e: ActionEvent): Unit = {
           var input = ""
-
-          val (valid, newState) = Controller(state, (inputField.getText(), turn))
-          if(!valid){
-            println("INVALID INPUT")
-            createInputWindow()
-            frame.dispose()
-          }
-          else{
+          val (valid, newState) =
+            Controller(state, (inputField.getText(), turn))
+          if (!valid) {
+            println("INVALID MOVE")
+            // createInputWindow()
+            // frame.dispose()
+          } else {
             println("VALID MOVE")
             Drawer(newState)
             state = newState
             turn = turn + 1
           }
-
           frame.dispose()
-
           createInputWindow()
-
           inputField.setText("")
         }
       })
 
-
-      //
       panel.add(submitButton)
-
       frame.add(panel, BorderLayout.CENTER)
       frame.setVisible(true)
     }
 
-
   }
 
-
-
-
 }
-
-
