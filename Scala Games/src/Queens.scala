6c57d4fb8f45c576d7cf0cbbf574c6421cc9ad9d
Ahmed Hesham <ahmedhesham.1652001@gmail.com>
2023-05-09 02:54:53 +0300

    Update Queens.scala

--- Scala Games/src/Queens.scala
+++ Scala Games/src/Queens.scala
@@ -11,56 +11,49 @@ import java.awt.{
 import java.io.File
 import javax.imageio.ImageIO
 import javax.swing.{BorderFactory, ImageIcon, JButton, JFrame, JPanel}
-import scala.collection.mutable.ArrayBuffer
 import javax.swing.{JFrame, JPanel, JButton, JLabel, ImageIcon, BorderFactory}
 
 object Queens {
+  def QueensInit(): Array[Int] = {
+    return Array.fill(42)(0)
+  }
   def QueensController(
-      state: ArrayBuffer[Int],
+      state: Array[Int],
       pair: (String, Int)
-  ): (Boolean, ArrayBuffer[Int]) = {
-    var tempState = ArrayBuffer.fill(64)(0)
-    if (state.length != 9) {
-      tempState = state
-    }
+  ): (Boolean, Array[Int], Int) = {
     val (input, turn) = pair
     val columns = Array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')
     if (
       !(columns.contains(input(1))) || !input(0).isDigit || !(input.length == 2)
     )
-      return (false, tempState)
+      return (false, state, turn)
 
     val x = input.substring(0, 1).toInt - 1
     val y = columns.indexOf(input(1), 0)
 
     if (x > 7 || y > 7)
-      return (false, tempState)
+      return (false, state, turn)
 
     for (i <- 0 to 63) {
-      if (tempState(i) == 1) {
+      if (state(i) == 1) {
         if (
           i % 8 == y || i / 8 == x || math.abs(i % 8 - y) == math.abs(i / 8 - x)
         )
-          return (false, tempState)
+          return (false, state, turn)
       }
     }
-    tempState(x * 8 + y) = 1
-    return (true, tempState)
+    state(x * 8 + y) = 1
+    return (true, state, turn)
   }
 
-  def QueensDrawer(state: ArrayBuffer[Int]): Unit = {
-    var tempState = ArrayBuffer.fill(64)(0)
-    if (state.length != 9) {
-      tempState = state
-    }
-
+  def QueensDrawer(state: Array[Int]): Unit = {
     val frame = new JFrame("8 Queens")
     frame.setSize(800, 600)
 
     val panel = new JPanel() {
       override def paint(gfx: Graphics) {
         super.paintComponent(gfx)
-        drawBoard(gfx, tempState)
+        drawBoard(gfx, state)
 
       }
     }
@@ -69,7 +62,7 @@ object Queens {
     frame.setVisible(true)
   }
 
-  private def drawBoard(gfx: Graphics, state: ArrayBuffer[Int]) {
+  private def drawBoard(gfx: Graphics, state: Array[Int]) {
     val writeColor = new Color(0, 0, 0)
     val black = new Color(128, 128, 128)
     val white = new Color(255, 255, 255)
